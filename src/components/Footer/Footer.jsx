efead8c3ba83c36d66e89c0cdf198cd83e52d2ce
Yurii <89393975+yur-dov@users.noreply.github.com>
2025-05-27 08:22:42 +0200

    add markup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import Container from "../Container/Container"
 function Footer() {
   return (
     <Container>
-
+      <ul>
+       <li>Home</li>
+       <li>Library</li>
+       <li>Favorite</li>
+      </ul>
     </Container>
   )
 }
