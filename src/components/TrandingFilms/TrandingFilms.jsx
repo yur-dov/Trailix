4d74a3a4d7ee8e2308630e70472052b72835ccb1
yur-dov <rapacta@gmail.com>
2025-06-05 19:31:16 +0200

    card film slider

--- src/components/TrandingFilms/TrandingFilms.jsx
+++ src/components/TrandingFilms/TrandingFilms.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchTrendingMovies, BASE_IMG_URL } from "../../services/ApiTMDB";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Container from "../Container/Container";
 import s from "./TrandingFilms.module.css";
 
@@ -11,6 +11,7 @@ function TrandingFilms() {
       try {
         const data = await fetchTrendingMovies();
         console.log(data);
+
         setFilms(data);
       } catch (error) {
         console.error("Error fetching trending movies:", error);
@@ -21,12 +22,18 @@ function TrandingFilms() {
   return (
     <Container className={s.addDesc}>
       <div className={s.titleWrapper}>
-      <h1 className={s.title}>TrandingFilms</h1>
-      <Link className={s.linkViewAll}>View all</Link>
+        <h1 className={s.title}>TrandingFilms</h1>
+        <Link className={s.linkViewAll}>View all</Link>
       </div>
       <ul className={s.trandList}>
         {films.map((film) => (
-          <li key={film.id} className={s.trandItem}><img src={`${BASE_IMG_URL}${film.poster_path}`} alt="" /></li>
+          <li key={film.id} className={s.trandItem}>
+            <img
+              src={`${BASE_IMG_URL}${film.poster_path}`}
+              alt=""
+              className={s.trandImg}
+            />
+          </li>
         ))}
       </ul>
     </Container>
