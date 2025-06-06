adc8f29f7d886c4426e020f34db01126ae6e3b19
yur-dov <rapacta@gmail.com>
2025-06-07 00:45:48 +0200

    genres filtered

--- src/pages/TrandingMovie/TrandingMovie.jsx
+++ src/pages/TrandingMovie/TrandingMovie.jsx
@@ -1,24 +1,61 @@
 import { useEffect, useState } from "react";
-import { fetchTrendingMovies } from "../../services/ApiTMDB";
+import {
+  fetchTrendingMovies,
+  getGenres,
+  BASE_IMG_URL,
+} from "../../services/ApiTMDB";
+import s from "./TrandingMovie.module.css";
+import Container from "../../components/Container/Container";
 
 function TrandingMovie() {
   const [films, setFilms] = useState([]);
   const [genres, setGenres] = useState([]);
+
   useEffect(() => {
-    const getTrandFilms = async () => {
+    const fetchData = async () => {
       try {
-        const data = await fetchTrendingMovies();
+        const [data, genres] = await Promise.all([
+          fetchTrendingMovies(),
+          getGenres(),
+        ]);
+        console.log(genres);
+
+        setGenres(genres);
         setFilms(data);
+        console.log(data);
       } catch (error) {
-        console.error("Error fetching trending movies:", error);
+        console.error("Error fetching genres/trending movies:", error);
       }
     };
-    getTrandFilms();
+
+    fetchData();
   }, []);
+
   return (
-    <div>
-      <h3></h3>
-    </div>
+    <Container>
+      <h2 className={s.TrandingMovieTitle}> Зараз у кіно</h2>
+      <ul className={s.movieList}>
+        {films.map((film) => (
+          <li key={film.id} className={s.movieItem}>
+            <img
+              src={`${BASE_IMG_URL}${film.poster_path}`}
+              alt=""
+              className={s.moviePoster}
+            />
+            <h3 className={s.MovieTitle}>{film.title}</h3>
+            <p className={s.MovieGenres}>
+              {film.genre_ids
+                .map((id) => {
+                  const genre = genres.find((g) => g.id === id);
+                  return genre ? genre.name : null;
+                })
+                .filter(Boolean) // убрать null если жанр не найден
+                .join(", ")}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </Container>
   );
 }
 export default TrandingMovie;
